6d93b109881baa4ae2be68ccf7e13e592b62adfa
David Markey <david@dmarkey.com>
2015-08-31 17:03:35 +0100

    Update Docs

--- comet_processor/push_back.py
+++ comet_processor/push_back.py
@@ -47,29 +47,66 @@ class TalkBackEvent(object):
 
     @property
     def ack_list(self):
+        """
+        The property that works out the list to send the acknowledgement to.
+        :return:
+        """
         return comet_config.REDIS_NAMESPACE + "ack_waiting/" + self.get_uuid()
 
     def ack(self, message="", status=200):
+        """
+        This function is used to acknowledge the initial request from the "handshake". It's important that
+        this is executed quickly.
+        :param message: The message to send back to the consumer
+        :param status: HTTP status code.
+        :return: Nothing
+        """
         wrapper = {'result_status': status, 'message': message, "uuid": self.get_uuid()}
         r.lpush(self.ack_list, json.dumps(wrapper, default=date_handler))
         self.ack_needed = False
 
     @staticmethod
     def from_uuid(uuid):
+        """
+        Use this static method to create a TalkBackEvent to send a message to the consumer.
+        :param uuid: the required UUID
+        :return: A TalkBackEvent object
+        """
         return TalkBackEvent({'uuid': uuid, "event": "push"})
 
     @staticmethod
     def send_message_bulk(self, uuids, message, status=200):
+        """
+        Given a list of uuids, send the same message back to each consumer.
+        :param uuids: list of uuids
+        :param message: the message thats required (object thats JSON serializable)
+        :param status: HTTP status code
+        :return:
+        """
         for uuid in uuids:
             self.from_uuid(uuid).send_messge(message, status)
 
     def serialize(self):
+        """
+        Serialize this object into JSON
+        :return: JSON object
+        """
         return json.dumps(self.request_data)
 
     def get_uuid(self):
+        """
+        Get the uuid of this object
+        :return: the UUID (String form)
+        """
         return self.request_data['uuid']
 
     def send_message(self, result, status=200):
+        """
+        Send a message to the TalkBack client
+        :param result: the message to be sent
+        :param status: the HTTP status code
+        :return:
+        """
         if self.ack_needed:
             raise AcknowledgementNeeded()
         wrapper = {'result_status': status, 'result_payload': result, "uuid": self.get_uuid()}
@@ -94,14 +131,28 @@ class TalkBackEvent(object):
         return r.eval(script, 3, uuid, payload, comet_config.REDIS_NAMESPACE)
 
     def finish(self):
+        """
+        Finish the session
+        :return: nothing
+        """
         self.send_message("session closed", status=410)
 
     def unauthorized(self, message=None):
+        """
+        Use this when a client is or has become unauthorized
+        :param message: Message to give if any
+        :return:
+        """
         if not self.ack_needed:
             return self.send_message(message, status=401)
         return self.ack(message, status=401)
 
     def bad_request(self, message=None):
+        """
+        Use this when the client has given a bad request
+        :param message: Message to give if any
+        :return: Nothing
+        """
         if not self.ack_needed:
             return self.send_message(message, status=400)
         return self.ack(message, status=400)
@@ -128,7 +179,19 @@ class IncomingProcessor(object):
 
         self.work(request)
 
+    def work(self, request):
+        """
+        Subclass this to consume each of the events coming in.
+        :param request: The incoming request in the form of a TalkBackEvent
+        :return: Nothing
+        """
+        raise NotImplementedError
+
     def run(self):
+        """
+        Execute this to start the processing
+        :return: nothing
+        """
         init()
         list_name = comet_config.REDIS_NAMESPACE + "incoming/" + self.service_name
         list_name_processing = list_name + "/processing"
